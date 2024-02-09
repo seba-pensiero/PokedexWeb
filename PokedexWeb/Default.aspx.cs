44cefdfa4feacb7fcfbc5bb1139be1ccf5b2f934
unknown <sebapensiero@gmail.com>
2024-02-09 18:35:21 +0100

    Listar con Foreach

--- PokedexWeb/Default.aspx.cs
+++ PokedexWeb/Default.aspx.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
 
 namespace PokedexWeb
 {
     public partial class Default1 : System.Web.UI.Page
     {
+        public List<Pokemon> ListaPokemon { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            PokemonNegocio negocio = new PokemonNegocio();
+            ListaPokemon = negocio.listarConSP();
 
         }
     }
---
+++ PokedexWeb/DetallePokemon.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="DetallePokemon.aspx.cs" Inherits="PokedexWeb.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
