66653d7df4af995991836fb419146d2c96b88004
unknown <sebapensiero@gmail.com>
2024-02-10 16:17:38 +0100

    Seteos proyecto App

---
+++ PokedexWeb/FormularioPokemon.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="FormularioPokemon.aspx.cs" Inherits="PokedexWeb.FormularioPokemon" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="row">
+        <div class="col-6">
+            <div class="mb-3">
+                <label for="txtId" class="form-label">Id</label>
+                <asp:textbox runat="server" ID="txtId" CssClass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="txtNombre" class="form-label">Nombre:</label>
+                <asp:textbox runat="server" ID="TextNombre" CssClass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="txtNumero" class="form-label">Número:</label>
+                <asp:textbox runat="server" ID="TexNumero" CssClass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="txtDescripcion" class="form-label">Descripción:</label>
+                <asp:textbox runat="server" TextMode="Multiline" ID="TextDescripcion" CssClass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="ddlTipo" class="form-label">Tipo:</label>
+                <asp:textbox runat="server" ID="ddlTipo" CssClass="form-select" />
+            </div>
+            <div class="mb-3">
+                <label for="ddlDebilidad" class="form-label">Debilidad:</label>
+                <asp:textbox runat="server" ID="ddlDebilidad" CssClass="form-select" />
+            </div>
+
+            <%--falta el evento onclick--%>
+            <div class="mb-3">
+                <asp:button text="Aceptar" ID="btnAceptar" CssClass="btn btn-primary" runat="server" />
+                <a href="PokemonLista.aspx">Cancelar</a>
+            </div>
+
+        </div>
+    </div>
+
+</asp:Content>
---
+++ PokedexWeb/FormularioPokemon.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PokedexWeb
+{
+
+
+    public partial class FormularioPokemon
+    {
+
+        /// <summary>
+        /// Control txtId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtId;
+
+        /// <summary>
+        /// Control TextNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextNombre;
+
+        /// <summary>
+        /// Control TexNumero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TexNumero;
+
+        /// <summary>
+        /// Control TextDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextDescripcion;
+
+        /// <summary>
+        /// Control ddlTipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ddlTipo;
+
+        /// <summary>
+        /// Control ddlDebilidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ddlDebilidad;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
+    }
+}
